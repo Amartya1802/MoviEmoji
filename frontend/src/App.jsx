38679e99c5d215cfe96ad9331c0adae04b851514
Amartya1802 <amartya21_ug@cse.nits.ac.in>
2024-06-26 03:33:09 +0530

    Initiated landing page

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import { LandingPage } from './components/landingPage/LandingPage'
+
+function App() {
+
+  return (
+    <>
+      <LandingPage />
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/components/landingPage/LandingPage.jsx
@@ -0,0 +1,17 @@
+import "./LandingPageStyle.css";
+
+export function LandingPage() {
+  return (
+    <>
+      <div className="title"> 
+          <h1>Home Title</h1>
+          <p>Welcome to the home page!</p>
+      </div>
+
+      <div className="lowHalf">
+          <h2>Bottom Half</h2>
+          <p>Some text here</p>
+      </div>
+    </>
+  )
+}
