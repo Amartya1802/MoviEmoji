5c0a6fe9ab33210052ae03ef723a8d501eef1f3f
Amartya1802 <amartya21_ug@cse.nits.ac.in>
2024-07-06 03:06:10 +0530

    landing page updated

--- frontend/src/components/landingPage/LandingPage.jsx
+++ frontend/src/components/landingPage/LandingPage.jsx
@@ -3,14 +3,31 @@ import "./LandingPageStyle.css";
 export function LandingPage() {
   return (
     <>
-      <div className="title"> 
-          <h1>Home Title</h1>
-          <p>Welcome to the home page!</p>
+      <div className="upHalf"> 
+        <div className="title">
+          <div className="titleText1">
+            <h1>😎MoviEmoji</h1>
+          </div>
+          <div className="titleText2">
+            <p>Guess the movie with emoji</p>
+          </div>
+        </div>
+        <div className="upperRight">
+          <h2>icon1</h2>
+          <h2>icon2</h2>
+          <h2>icon3</h2>
+          <h2>icon4</h2>
+        </div>
       </div>
 
       <div className="lowHalf">
-          <h2>Bottom Half</h2>
-          <p>Some text here</p>
+        <div className="makeRoom">
+          <button className="makeRoomBtn">Create Room</button>
+        </div>
+        <div className="joinRoom">
+          <input type="text" className="roomLinkInput" placeholder="Paste room link here" />
+          <button className="joinRoomBtn">Join Room</button>
+        </div>
       </div>
     </>
   )
